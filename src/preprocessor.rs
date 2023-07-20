587b07f282d0f9d32622ca0f9cf34672f080af82
Benjamin Eder <ben-eder@freenet.de>
2023-07-20 21:44:56 +0200

    feat: process chapters

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,7 +1,8 @@
 use mdbook::{
-    book::Book,
+    book::{Book, Chapter},
     errors::Result,
     preprocess::{Preprocessor, PreprocessorContext},
+    BookItem,
 };
 
 pub struct MindmapPreprocessor;
@@ -18,10 +19,19 @@ impl Preprocessor for MindmapPreprocessor {
     }
 
     fn run(&self, _: &PreprocessorContext, book: Book) -> Result<Book> {
-        Result::Ok(book)
+        let mut processed_book = book.clone();
+
+        processed_book.for_each_mut(|item| match item {
+            BookItem::Chapter(chapter) => process_chapter(chapter),
+            _ => {}
+        });
+
+        Result::Ok(processed_book)
     }
 
     fn supports_renderer(&self, _renderer: &str) -> bool {
         _renderer == "html"
     }
 }
+
+fn process_chapter(_: &mut Chapter) {}
