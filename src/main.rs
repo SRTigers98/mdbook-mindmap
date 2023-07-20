02ff8f184e049b4386a8ccbc97519e149d141925
Benjamin Eder <ben-eder@freenet.de>
2023-07-20 20:22:30 +0200

    feat: handle cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use clap::{Arg, Command};
+use std::{io, process};
+
+use clap::{Arg, ArgMatches, Command};
+use mdbook::{
+    errors::Error,
+    preprocess::{CmdPreprocessor, Preprocessor},
+};
+use mdbook_mindmap::preprocessor::MindmapPreprocessor;
 
 fn make_app() -> Command {
     Command::new("mdbook-mindmap")
@@ -13,9 +20,33 @@ fn make_app() -> Command {
 fn main() {
     let matches = make_app().get_matches();
 
-    if let Some(sub_args) = matches.subcommand_matches("supports") {
-        print!("supports command called!")
-    } else {
-        print!("root command called!")
+    let preprocessor = MindmapPreprocessor::new();
+
+    if let Some(args) = matches.subcommand_matches("supports") {
+        handle_supports(&preprocessor, args);
+    } else if let Err(e) = handle_preprocessing(&preprocessor) {
+        eprintln!("{}", e);
+        process::exit(1);
     }
+}
+
+fn handle_supports(pre: &dyn Preprocessor, args: &ArgMatches) -> ! {
+    let renderer = args
+        .get_one::<String>("renderer")
+        .expect("required argument");
+
+    if pre.supports_renderer(renderer) {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    let processed_book = pre.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book)?;
+
+    Ok(())
 }
