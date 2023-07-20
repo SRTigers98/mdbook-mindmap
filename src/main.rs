82e8313d3dac75a6241fef9de4fd1ad081b5c216
Benjamin Eder <ben-eder@freenet.de>
2023-07-20 19:22:53 +0200

    feat: create preprocessor cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Arg, Command};
+
+fn make_app() -> Command {
+    Command::new("mdbook-mindmap")
+        .about("A mdbook preprocessor to create mindmaps from the file structure")
+        .subcommand(
+            Command::new("supports")
+                .arg(Arg::new("renderer").required(true))
+                .about("Checks if the renderer is supported"),
+        )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = make_app().get_matches();
+
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        print!("supports command called!")
+    } else {
+        print!("root command called!")
+    }
 }
